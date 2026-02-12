d73e3a0041ceabd6b430d2777eb658d89aa36de6
jbolivar2 <jbolivara@unal.edu.co>
2026-02-12 09:46:20 -0500

    Update Ejrcicio 12.py

--- Ejrcicio 12.py
+++ Ejrcicio 12.py
@@ -1,21 +1,20 @@
-class Salario:
+class Empleado:
+
     def __init__(self, horas, valor_hora):
         self.horas = horas
         self.valor_hora = valor_hora
-        self.retencion_porcentaje = 0.125
 
-    def salario_bruto(self):
-        return self.horas * self.valor_hora
+    def calcular_salario(self):
+        self.salario_bruto = self.horas * self.valor_hora
+        self.retencion = self.salario_bruto * 0.125
+        self.salario_neto = self.salario_bruto - self.retencion
 
-    def retencion(self):
-        return self.salario_bruto() * self.retencion_porcentaje
+    def mostrar_datos(self):
+        print("Salario bruto:", self.salario_bruto)
+        print("Retención:", self.retencion)
+        print("Salario neto:", self.salario_neto)
 
-    def salario_neto(self):
-        return self.salario_bruto() - self.retencion()
 
-
-empleado = Salario(48, 5000)
-
-print("Salario bruto:", empleado.salario_bruto())
-print("Retención:", empleado.retencion())
-print("Salario neto:", empleado.salario_neto())
+empleado1 = Empleado(48, 5000)
+empleado1.calcular_salario()
+empleado1.mostrar_datos()
