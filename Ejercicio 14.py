86f8ec4c21520ad0cded1f042872c501d46a8122
jbolivar2 <jbolivara@unal.edu.co>
2026-02-12 09:52:43 -0500

    Update Ejercicio 14.py

--- Ejercicio 14.py
+++ Ejercicio 14.py
@@ -8,11 +8,13 @@ class Numero:
         self.cubo = self.numero ** 3
 
     def mostrar(self):
+        print("\nResultados:")
         print("Número:", self.numero)
         print("Cuadrado:", self.cuadrado)
         print("Cubo:", self.cubo)
 
 
-num = Numero(5)
-num.calcular()
-num.mostrar()
+num = float(input("Ingrese un número: "))
+n = Numero(num)
+n.calcular()
+n.mostrar()
