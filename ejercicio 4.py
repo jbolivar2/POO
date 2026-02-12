c931c9d2eca9c98276a572bd00f1d27e93e40397
jbolivar2 <jbolivara@unal.edu.co>
2026-02-12 09:45:16 -0500

    Update ejercicio 4.py

--- ejercicio 4.py
+++ ejercicio 4.py
@@ -1,21 +1,20 @@
-class Familia:
-    def __init__(self, edad_juan):
-        self.edad_juan = edad_juan
+class EdadesFamilia:
 
-    def edad_alberto(self):
-        return (2 / 3) * self.edad_juan
+    def __init__(self):
+        self.edad_juan = 3
 
-    def edad_ana(self):
-        return (4 / 3) * self.edad_juan
+    def calcular_edades(self):
+        self.edad_alberto = (2/3) * self.edad_juan
+        self.edad_ana = (4/3) * self.edad_juan
+        self.edad_mama = self.edad_juan + self.edad_alberto + self.edad_ana
 
-    def edad_mama(self):
-        return self.edad_juan + self.edad_alberto() + self.edad_ana()
+    def mostrar_edades(self):
+        print("Edad de Juan:", self.edad_juan)
+        print("Edad de Alberto:", self.edad_alberto)
+        print("Edad de Ana:", self.edad_ana)
+        print("Edad de la mamá:", self.edad_mama)
 
-edad_juan = float(input("Ingrese la edad de Juan: "))
 
-familia = Familia(edad_juan)
-
-print("Edad de Juan:", edad_juan)
-print("Edad de Alberto:", familia.edad_alberto())
-print("Edad de Ana:", familia.edad_ana())
-print("Edad de la mamá:", familia.edad_mama())
+familia = EdadesFamilia()
+familia.calcular_edades()
+familia.mostrar_edades()
